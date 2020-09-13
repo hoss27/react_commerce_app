15c349185214343b50d62b705c4de13e0c5e8330
ahmed_27 <ahmed.hamouda0@gmail.com>
2020-09-13 16:24:15 +0200

    styling-pages

---
+++ src/pages/home/home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./home.scss";
+import DirectoryMenu from "../../components/directory-menu";
+const Home = () => {
+  return (
+    <div className="homepage">
+      <DirectoryMenu/>
+    </div>
+  );
+};
+
+export default Home;
