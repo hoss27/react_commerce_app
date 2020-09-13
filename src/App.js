6e303abd9592bfe315c3fa27ab8f66d511d651b1
ahmed_27 <ahmed.hamouda0@gmail.com>
2020-09-13 23:53:39 +0200

    header-component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Home from "./pages/home/home";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import ShopPage from "./pages/shop/shop";
+import Header from "./pages/header/header";
 
 function App() {
   return (
     <div>
+      <Header/>
+      <Switch>
       <Route exact path="/" component={Home} />
       <Route path="/shop" component={ShopPage} />
+    </Switch>
     </div>
   );
 }
